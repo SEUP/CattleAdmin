6d675977e9cc6d1a3aa773e7c5a5ea2e94ce14ce
Chaow Porkaew <chaow.po@up.ac.th>
2018-05-21 16:04:55 +0700

    update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,29 @@
 <template>
-  <div>
-    CURRENCY :
-    <select v-model="selectedCurrency" @change="updateData">
-      <option v-for="currency in currencies">{{currency}}</option>
-    </select>
-
-    <template v-if="output">
-      {{output}}
-    </template>
-  </div>
+  <v-app>
+    <v-container>
+<v-layout column> 
+  <v-flex>
+    <v-select
+          :items="currencies"
+          v-model="selectedCurrency"
+          label="กรุณาเลือกสกุลเงิน"
+          single-line
+           @change="updateData"
+        ></v-select>
+  </v-flex>
+  <v-flex v-if="output">
+     <v-list two-line subheader>
+          <v-list-tile v-for="(value,key) in output.rates">
+            <v-list-tile-content>
+              <v-list-tile-title>{{key}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{value}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+      </v-list>
+  </v-flex>
+</v-layout>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -26,12 +41,12 @@
     },
     methods: {
       updateData: async function (event) {
-        // console.log(event,this.selectedCurrency);
+         console.log(event);
 
         let result = await axios.get("http://data.fixer.io/api/latest", {
           params: {
             access_key: "69e2a60ca026b20ae9bca7a62a57dddc",
-            symbols: this.selectedCurrency,
+            symbols: event,
           }})
           .then( (response) => {
             console.log(response);
@@ -41,7 +56,7 @@
             return null;
           })
         if(result){
-          this.output = result;
+          this.output = result.data;
         }
 
       }
