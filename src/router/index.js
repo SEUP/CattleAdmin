a8c085af9feb2785f1690b1e7bb53d028c3a872a
sagotty <37680268+sagotty@users.noreply.github.com>
2018-05-30 15:33:50 +0700

    เขียนเวปแมวครับ

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login  from '@/components/Login'
 import Home  from '@/components/Home'
-
+import Eiei from '@/components/Eiei'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +15,10 @@ export default new Router({
       path: '/Home',
       name: 'Home',
       component: Home
+    }, {
+      path: '/Eiei',
+      name: 'Eiei',
+      component: Eiei
     }
   ]
 })
