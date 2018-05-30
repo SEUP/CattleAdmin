fe121e78c6c45b03eeee8a548b4643b29fa5babb
porrametict <porrametict@gmil.com>
2018-05-30 15:43:51 +0700

    Merge branch 'master' of D:\CDProject\CattleAdmin with conflicts.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Login  from '@/components/Login'
 import Home  from '@/components/Home'
 import Nook  from '@/components/Nook'
+import Porramet  from '@/components/Porramet'
 
 Vue.use(Router)
 
@@ -20,6 +21,10 @@ export default new Router({
       path: '/Nook',
       name: 'Nook',
       component: Nook
+    }, {
+      path: '/Porramet',
+      name: 'Porramet',
+      component: Porramet
     }
   ]
 })
