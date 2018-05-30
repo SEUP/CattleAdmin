8b7e4b80abcc3c364e137164601d52979d1e9c7d
Chaow Porkaew <chaow.po@up.ac.th>
2018-05-30 13:38:16 +0700

    add chaow

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login  from '@/components/Login'
 import Home  from '@/components/Home'
+import Chaow  from '@/components/Chaow'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
       path: '/Home',
       name: 'Home',
       component: Home
+    },{
+      path: '/chaow',
+      name: 'chaow',
+      component: Chaow
     }
   ]
 })
