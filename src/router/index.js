39ee72981176e373596f17285671a136f96cee3c
Chaow Porkaew <chaow.po@up.ac.th>
2018-05-31 13:25:56 +0700

    remove unuse files

--- src/router/index.js
+++ src/router/index.js
@@ -1,51 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login  from '@/components/Login'
-import Home  from '@/components/Home'
-import Chaow  from '@/components/Chaow'
-import Most  from '@/components/Most'
-import Nook  from '@/components/Nook'
-import Porramet  from '@/components/Porramet'
-import pai from '@/components/paikung'
-import Eiei from '@/components/Eiei'
 
-Vue.use(Router)
+function view(name) {
+  return function (resolve) {
+    require(['@/components/' + name + '.vue'], resolve);
+  }
+}
+
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Login',
-      component: Login
+      component: view("Login")
     }, {
       path: '/Home',
       name: 'Home',
-      component: Home
-    },{
-      path: '/chaow',
-      name: 'chaow',
-      component: Chaow
-    }, {
-      path: '/Nook',
-      name: 'Nook',
-      component: Nook
-    }, {
-      path: '/Porramet',
-      name: 'Porramet',
-      component: Porramet
-    }, {
-      path: '/Eiei',
-      name: 'Eiei',
-      component: Eiei
-    }, {
-      path: '/Most',
-      name: 'Most',
-      component: Most
+      component: view("Home")
     }
-
-, {
-  path: '/paikung',
-  name: 'pai',
-  component: pai}
   ]
 })
