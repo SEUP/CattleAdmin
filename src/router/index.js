d6bfc82de79a72a13e1fd392b52524d682420189
sagotty <37680268+sagotty@users.noreply.github.com>
2018-05-31 13:54:02 +0700

    Merge branch 'master' of E:\workSE\TestWS\cattleadmin with conflicts.

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+import Chaow from '@/components/Chaow'
+import Most from '@/components/Most'
+import Nook from '@/components/Nook'
+import Porramet from '@/components/Porramet'
+import pai from '@/components/paikung'
+import Eiei from '@/components/Eiei'
 
-function view(name) {
-  return function (resolve) {
-    require(['@/components/' + name + '.vue'], resolve);
-  }
-}
-
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Login',
-      component: view("Login")
+      component: view('Login')
     }, {
       path: '/Home',
       name: 'Home',
-      component: view("Home")
+      component: Home
     }
-  ]
-})
