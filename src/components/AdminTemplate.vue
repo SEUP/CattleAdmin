934c30266aafe6a0561d208167f8cf3e336b7445
Chaow Porkaew <chaow.po@up.ac.th>
2018-05-31 14:05:12 +0700

    update chaow.vue

--- src/components/AdminTemplate.vue
+++ src/components/AdminTemplate.vue
@@ -7,7 +7,7 @@
       app
     >
       <v-list dense>
-        <v-list-tile>
+        <v-list-tile :to="{name : 'admin-home'}" exact>
           <v-list-tile-action>
             <v-icon>mdi-home</v-icon>
           </v-list-tile-action>
@@ -17,12 +17,23 @@
             </v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
+
+        <v-list-tile :to="{name : 'admin-chaow'}" exact>
+          <v-list-tile-action>
+            <v-icon>mdi-account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              Chaow
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
       :clipped-left="$vuetify.breakpoint.lgAndUp"
       color="blue darken-3" dark app fixed>
-      <v-toolbar-title  class="ml-0 pl-3 mr-3">
+      <v-toolbar-title class="ml-0 pl-3 mr-3">
         <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
         <span class="hidden-sm-and-down">Cattle Information System</span>
       </v-toolbar-title>
