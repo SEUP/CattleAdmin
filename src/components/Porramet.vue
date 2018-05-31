f21c19fef59859eab75393b2678549ce918884cc
porrametict <porrametict@gmil.com>
2018-05-31 14:10:46 +0700

    admin-porramet

---
+++ src/components/Porramet.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fluid>
+    <v-layout>
+      <h1>Porramet</h1>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Porramet"
+    }
+</script>
+
+<style scoped>
+
+</style>
