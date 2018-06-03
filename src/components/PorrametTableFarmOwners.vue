3c708eb8d0fa61da951e8d7a81efa8bbecf17ae8
porrametict <porrametict@gmil.com>
2018-06-03 08:15:09 +0700

    edit actions farm/loader

--- src/components/PorrametTableFarmOwners.vue
+++ src/components/PorrametTableFarmOwners.vue
@@ -1,19 +1,24 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="dataItems"
+    :items="tableDataFarmOners"
     hide-actions
     class="elevation-1"
   >
     <template slot="items" slot-scope="props">
       <td>{{ props.item.id }}</td>
-      <td class="text-xs-left">{{ props.item.first_name }}</td>
-      <td class="text-xs-left">{{ props.item.last_name }}</td>
+      <td class="text-xs-left pa-0 ma-0">{{ props.item.first_name }}</td>
+      <td class="text-xs-left ">{{ props.item.last_name }}</td>
       <td class="text-xs-left">{{ props.item.mobile_no }}</td>
       <td class="text-xs-left">{{ props.item.province_name }}</td>
       <td class="text-xs-left">{{ props.item.amphur_name }}</td>
       <td class="text-xs-left">{{ props.item.district_name }}</td>
       <td class="text-xs-left">{{ props.item.updated_at }}</td>
+      <td class="text-xs-left">
+        <v-btn class="ma-0" icon v-for="at in props.item.action" :key="at.name">
+          <v-icon :color="at.color" >{{at.icon}}</v-icon>
+        </v-btn>
+      </td>
     </template>
   </v-data-table>
 </template>
@@ -30,44 +35,24 @@
           {text: "อำเภอ", align:"left" ,value:"amphur_name"},
           {text: "ตำบล", align:"left" ,value:"district_name"},
           {text: "ข้อมูลเมื่อ", align:"left" ,value:"updated_at"},
-        ],
-        dataItems: [
+          {text: "การจัดการ", align:"left" ,value:"action"},
 
-        ]
+        ],
       }
     },computed : {
-      DataFarmOners() {
-        return this.$store.state.farmowner.farmOwners;
+      tableDataFarmOners() {
+        return this.$store.state.farmowner.tableFarmOwners;
       }
     }, async created() {
       await  this.loadData();
-      await console.log(1)
-      await  this.loadDataToTable()
     },methods : {
       loadData: async function () {
         await this.$store.dispatch("farmowner/loader");
-        await console.log(2)
+        // await console.log(this.tableDataFarmOners)
+
       },
       display : function (data) {
-        this.DataFarmOners = data
-      },loadDataToTable : async  function () {
-        let i ;
-        await console.log(3)
-        for(i=0;i<this.DataFarmOners.length;i++){
-          let nowData = this.DataFarmOners[i]
-          let newData = {
-            value:false,
-            id:nowData.id,
-            first_name:nowData.first_name
-            ,last_name:nowData.last_name,
-            mobile_no:nowData.mobile_no,
-            province_name: nowData.province_name,
-            amphur_name:nowData.amphur_name
-            ,district_name:nowData.district_name,
-            updated_at:nowData.updated_at}
-          this.dataItems.push(newData)
-          console.log(88)
-        }
+        this.tableDataFarmOners = data
       }
     }
   }
--- src/stores/modules/farmowner.js
+++ src/stores/modules/farmowner.js
@@ -1,27 +1,57 @@
 export  default {
   namespaced : true,
   state : { //data
-    farmOwners :[
-      {id :""
-      ,first_name: ""
-      ,last_name : ""}
-    ]
+    farmOwners :[],
+    tableFarmOwners : []
   },mutations : {
         setfarmOwners : function (state,farmOwnersData)  {
           state.farmOwners = farmOwnersData
+        },
+        settablefarmOwners : function (state,dataTable) {
+          state.tableFarmOwners = dataTable
         }
+
   },actions : {
         loader : async function (context) {
           await axios.get("http://mct.ict.up.ac.th:10007/api/farm-owner")
             .then( (r) => {
               let farmOwnersData  = r.data.data;
               context.commit("setfarmOwners",farmOwnersData)
+              let farmOwnerData = this.farmOwners;
+              // console.log(farmOwnersData)
+              let i;
+              let table = [];
+              for(i=0;i<farmOwnersData.length;i++){
+                let nowData = farmOwnersData[i]
+                let newData = {
+                  value:false,
+                  id:nowData.id,
+                  first_name:nowData.first_name
+                  ,last_name:nowData.last_name,
+                  mobile_no:nowData.mobile_no,
+                  province_name: nowData.province_name,
+                  amphur_name:nowData.amphur_name
+                  ,district_name:nowData.district_name,
+                  updated_at:nowData.updated_at,
+                  action : [
+                    {name:"export",icon:"unarchive",color:"success"},
+                    {name:"edit",icon:"create",color:"primary"},
+                    {name:"delete",icon:"delete",color:"red"},
+                  ]
+                }
+
+                table.push(newData)
+                // console.log(newData)
+                // console.log(5555)
+              }context.commit("settablefarmOwners",table)
+              // console.log(table)
+
+
 
             })
             .catch( (e) => {
 
             })
-          console.log('55')
-        }
-  }
+        },
+}
 }
