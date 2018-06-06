4484c236f9afc8696445b26186117e45967a38a2
ford19982541 <33016279+ford19982541@users.noreply.github.com>
2018-06-06 16:29:53 +0700

    add user component

--- src/components/user/index.vue
+++ src/components/user/index.vue
@@ -8,7 +8,7 @@
       <v-divider class="my-3"></v-divider>
       <v-layout row>
         <v-flex xs3 >
-          <v-btn color="primary">Accept <v-icon>mdi-account-plus</v-icon>
+          <v-btn href=".#/admin/user/create" color="primary">ADD <v-icon>mdi-account-plus</v-icon>
           </v-btn>
         </v-flex>
         <v-flex xs7 offset-xs6>
@@ -17,6 +17,8 @@
             name="input-1"
             label="ค้นหา"
             append-icon="mdi-magnify"
+            v-model="text"
+            @keyup.13="search()"
           ></v-text-field>
         </v-flex>
       </v-layout>
@@ -25,17 +27,17 @@
           <v-card>
             <v-data-table
               :headers="headers"
-              :items="item"
+              :items="Data"
               hide-actions
               class="elevation-1"
             >
               <template slot="items" slot-scope="props">
                 <td>{{ props.item.name }}</td>
                 <td>{{ props.item.email }}</td>
-                <td>{{ props.item.role }}</td>
+                <td>{{ props.item.username}}</td>
                 <td>{{ props.item.atdes}}</td>
                 <td>{{ props.item.action }}<v-btn  color="success" @click>Edit</v-btn>
-                  <v-btn  color="error"@click>Delete</v-btn></td>
+                  <v-btn  color="error"@click="dele()">Delete</v-btn></td>
               </template>
             </v-data-table>
             <div class="text-xs-center">
@@ -51,26 +53,33 @@
 <script>
   export default {
     data: () => ({
-      headers:[
-        {text: "name", align:"left" ,value:"name"},
-        {text: "Email", align:"left" ,value:"email"},
-        {text: "Role", align:"left" ,value:"role"},
-        {text: "จังหวัด อำเภอ ตำบล", align:"left" ,value:"atdes"},
-        {text: "Action", align:"left" ,value:"action"},
+      headers: [
+        {text: "name", align: "left", value: "name"},
+        {text: "Email", align: "left", value: "email"},
+        {text: "Username", align: "left", value: "role"},
+        {text: "จังหวัด อำเภอ ตำบล", align: "left", value: "atdes"},
+        {text: "Action", align: "left", value: "action"},
 
 
       ],
-      sheet: false,
-      tiles: [
-        { img: 'keep.png', title: 'Keep' },
-        { img: 'inbox.png', title: 'Inbox' },
-        { img: 'hangouts.png', title: 'Hangouts' },
-        { img: 'messenger.png', title: 'Messenger' },
-        { img: 'google.png', title: 'Google+' }
+      item: [{
+        name: "paikung", email: "paikung@root", role: "admin", atdes: "เมือง นางเเล เชียงราย"
+      }]}),
+      computed: {
+        Data() {
+          return this.$store.state.users.usersData
+        }
+      }, async created() {
+        await  this.loadData()
+      }, methods: {
+        loadData: async function () {
+          await this.$store.dispatch('users/loadUsers')
+          await console.log(this.Data);
+        },search : async function () {
+          await  this.$store.dispatch("users/searchUsers", this.text)
+        }
+        }
 
-      ],item :[{name:"paikung",email:"paikung@root", role:"admin" ,atdes: "เมือง นางเเล เชียงราย"
-      }]
-    })
   }
 </script>
 
