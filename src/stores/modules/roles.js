5b493d33748cc4a1f857538c3973b775ff18cb3c
Chaow Porkaew <chaow.po@up.ac.th>
2018-06-06 16:40:13 +0700

    fix roles vuex

--- src/stores/modules/roles.js
+++ src/stores/modules/roles.js
@@ -1,26 +1,35 @@
 export default {
-  namespaced : true,
-  state : {
-    rolesData :[],
-  },mutations : {
-    setRolesData : function (state,DataRoles) {
+  namespaced: true,
+  state: {
+    paginate: [],
+    rolesData: [],
+  }, mutations: {
+    setPaginate: function (state, Paginate) {
+      state.paginate = Paginate;
+      state.rolesData = Paginate.data;
+    },
+    setRolesData: function (state, DataRoles) {
       state.rolesData = DataRoles
     },
-  },actions : {
-    loadRoles : async function (context) {
-      await axios.get("http://mct.ict.up.ac.th:10007/api/roles")
-        .then( (r) => {
-          let data = r.data.data;
-          context.commit("setRolesData",data)
+  }, actions: {
+    loadRoles: async function (context, params = null) {
+      await axios.get("http://mct.ict.up.ac.th:10007/api/roles",
+        {params: params})
+        .then((r) => {
+          if (params && !params.paginate) {
+            context.commit("setRolesData", r.data)
+          } else {
+            context.commit("setPaginate", r.data)
+          }
+        })
+        .catch((err) => {
 
         })
-        .catch( (err) => {
-
-        })
-    },searchRoles : async function (context,data) {
-      axios.get("http://mct.ict.up.ac.th:10007/api/roles",{ params : { keyword: data } })
-        .then( (r) => {
-          context.commit("setRolesData",r.data.data)
+    },
+    searchRoles: async function (context, data) {
+      axios.get("http://mct.ict.up.ac.th:10007/api/roles", {params: {keyword: data}})
+        .then((r) => {
+          context.commit("setRolesData", r.data.data)
           console.log(r.data.data)
         })
 
