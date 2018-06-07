2f327e9533c4a1c2e3b01299750614d8709d659f
Chaow Porkaew <chaow.po@up.ac.th>
2018-06-07 12:27:38 +0700

    fix roles

--- src/stores/modules/roles.js
+++ src/stores/modules/roles.js
@@ -1,35 +1,18 @@
-export  default {
-  namespaced : true ,
-  state : {
-    roleData : {},
-    rolesData :[],
-    rolesPaginate : {},
-  },
-  mutations :{
-    setRolesData  : function (state,roles) {
-      state.rolesData = roles
+export default {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {
+    async getRoles(context, params = null) {
+      let result = await axios.get("/api/roles", {params: params})
+        .then((response) => {
+          return response.data;
+        })
+        .catch((err) => {
+          return null
+        });
+      return result
     },
-    setRolesPaginate : function (state,paginate) {
-      state.rolesPaginate = paginate;
-      state.rolesData = paginate.data
-    }
-  },
-  actions  : {
-      async getRoles (context,params = null) {
-       let r =  await axios.get("/api/roles", {params: params})
-          .then( (r) => {
-            if (params && !params.paginate){
-              context.commit("setRolesData",r.data)
-            } else  {
-              context.commit("setRolesPaginate",r.data)
-            }
-            return r.data
-          })
-          .catch( (err) => {
-            return null
-          });
-        return r
-      },
   }
 
 //
