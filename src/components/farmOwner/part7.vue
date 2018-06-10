e86b3c41dbbd44c1b32221cc05efe8bda141ba85
ford <ford19982541@gmail.com>
2018-06-10 12:52:27 +0700

    updata 7 in addFarmOwner

--- src/components/farmOwner/part7.vue
+++ src/components/farmOwner/part7.vue
@@ -1,58 +1,24 @@
 <template>
   <v-card>
-    <v-layout row>
-      <v-flex xs12>
-        <p class="title">7.1 หน่วยงานที่ให้บริการส่งเสริมการเลี้ยงโคเนื้อ(ตอบได้มากกว่า 1 ข้อ)</p>
-        <v-divider class="my-0"></v-divider>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs12>
-        <p class="title">7.2 ท่านเคยได้รับการเยี่ยมเยือนฟาร์มจากหน่วยงานในข้อ 7.1 หรือไม่</p>
-        <v-divider class="my-0"></v-divider>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs12>
-        <p class="title">7.3 ท่านเคยได้รับการสนับสนุนปัจจัยการผลิตหรือไม่</p>
-        <v-divider class="my-0"></v-divider>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs12>
-        <p class="title">7.4 การใช้บริการด้านสุขภาพโคจากหน่วยงานของกรมปศุสัตว์</p>
-        <v-divider class="my-0"></v-divider>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs12>
-        <p class="title">7.5 ท่านเคยเข้ารับการฝึกอบรมเกี่ยวกับการเลี้ยงโคเนื้อหรือไม่</p>
-        <v-divider class="my-0"></v-divider>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs12>
-        <p class="title">7.6 ท่านเคยไปดูงานเกี่ยวกับการเลี้ยงโคเนื้อหรือไม่</p>
-        <v-divider class="my-0"></v-divider>
-      </v-flex>
-    </v-layout>
-    <v-layout row>
-      <v-flex xs12>
-        <p class="title">7.7 ท่านเคยได้รับการสนับสนุนให้แม่พันธุ์โคเนื้อมาเลี้ยงเพื่อผลิตลูกหรือไม่</p>
-        <v-divider class="my-0"></v-divider>
-        <!--<choice-select :value="form.sex" type="sex" label="sex"></choice-select>-->
-      </v-flex>
-    </v-layout>
+    <v-container>
+      <v-form>
+        <v-flex xs12>
+          <p class="title">7.7 ท่านเคยได้รับการสนับสนุนให้แม่พันธุ์โคเนื้อมาเลี้ยงเพื่อผลิตลูกหรือไม่</p>
+          <v-divider class="my-3"></v-divider>
+          <choice-select type="female_breeder_support" ></choice-select>
+        </v-flex>
+      </v-form>
+    </v-container>
   </v-card>
 </template>
 
 <script>
   import choiceSelect from "@/components/share/choiceSelect";
+  import ChoiceSelect from "../share/choiceSelect";
     export default {
-        name: "part7",
-      component :{
-          choiceSelect
-      },
+      components: {choiceSelect},
+      name: "part7",
+
       data(){
           return{
             selected:[]
