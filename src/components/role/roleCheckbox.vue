e261c8b2fa84f199b93d98090cc398bb4c89a20c
Chaow Porkaew <chaow.po@up.ac.th>
2018-06-13 11:48:05 +0700

    fix user edit

--- src/components/role/roleCheckbox.vue
+++ src/components/role/roleCheckbox.vue
@@ -1,61 +1,62 @@
 <template>
-    <div>
-        <div class="checkbox" v-for="role in roles">
-            <v-checkbox :label="role.name"
-                        v-bind:value="role"
-                        v-on:change="$emit('change', role)"
-                        :input-value="value"
-            >
-            </v-checkbox>
-        </div>
+  <div v-if="isReady">
+    <div class="checkbox" v-for="role in roles">
+      <v-checkbox :label="role.name"
+                  v-bind:value="role"
+                  v-on:change="$emit('change', role)"
+                  :input-value="value"
+      >
+      </v-checkbox>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "role-checkbox",
-        props: {
-            value: {
-                type: [Array],
-                default: () => []
-            }
-        },
-        data() {
-            return {
-                roles: [],
-                selected: [],
-            }
-        },
-        methods: {
-            sync: function () {
-                let roles_length = this.roles.length;
-                let sel_length = this.value.length;
+  export default {
+    name: "role-checkbox",
+    props: {
+      value: {
+        type: [Array],
+        default: () => []
+      }
+    },
+    data() {
+      return {
+        isReady: false,
+        roles: [],
+        selected: [],
+      }
+    },
+    methods: {
+      sync: function () {
+        let roles_length = this.roles.length;
+        let sel_length = this.value.length;
 
-                let roles = this.roles;
-                let sel = this.value;
+        let roles = this.roles;
+        let sel = this.value;
 
-                for (let i = 0; i < roles_length; i++) {
-                    for (let j = 0; j < sel_length; j++) {
-                        if (roles[i].id == sel[j].id) {
-                            roles[i] = sel[j]
-                        }
-                    }
-                }
-            },
-            load: async function () {
-                this.roles = await this.$store.dispatch('roles/getRoles',{paginate : false});
-                this.sync();
-                console.log("checkbox",this.roles);
+        for (let i = 0; i < roles_length; i++) {
+          for (let j = 0; j < sel_length; j++) {
+            if (roles[i].id == sel[j].id) {
+              roles[i] = sel[j]
             }
-        },
-        created: function () {
-            this.load();
-        },
-        mounted: function () {
-            console.log('role-checkbox mounted')
+          }
         }
-
+      },
+      load: async function () {
+        this.roles = await this.$store.dispatch('roles/getRoles', {paginate: false});
+        this.sync();
+        this.isReady = true;
+      }
+    },
+    created: function () {
+      this.load();
+    },
+    mounted: function () {
+      console.log('role-checkbox mounted')
     }
+
+  }
 </script>
 
 <style scoped>
