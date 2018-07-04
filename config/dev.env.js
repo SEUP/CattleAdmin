49849299ca8ee00416c1eae75c09633f053f5d4c
Chaow Porkaew <chaow.po@up.ac.th>
2018-07-04 16:28:33 +0700

    update package.json

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,14 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_URL: '"http://mct.ict.up.ac.th:10008"',
-  //API_URL : '"http://10.94.1.113:10002"',
+  // test - server
+  // API_URL : '"http://10.94.3.127:10002"',
+  // CLIENT_ID : '5',
+  // CLIENT_SECRET : '"rcbRlgZjkk3zJMfhYITKK4EMTCh1rBnBcYgNaJiq"'
+
+  //online - server
+  API_URL: '"http://mct.ict.up.ac.th:10007"',
+  CLIENT_ID : '8',
+  CLIENT_SECRET : '"sGtSeZdxfjHmto8FVdDxG0kTK2b3JYWJtfVaKgOL"'
+
 })
