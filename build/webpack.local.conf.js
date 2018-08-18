3a291796f580bda567dc309d85b9b37a34f6c9fb
Chaow Porkaew <chaow.po@up.ac.th>
2018-08-18 20:53:19 +0700

    add local and server dev scripts

--- build/webpack.local.conf.js
+++ build/webpack.local.conf.js
@@ -46,7 +46,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': require('../config/devtest.env')
+      'process.env': require('../config/local.env')
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,4 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_URL: '"http://10.94.10.142:3333"',
-  // API_URL: '"http://mct.ict.up.ac.th:10008"'
 })
